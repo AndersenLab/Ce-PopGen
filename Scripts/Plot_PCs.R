d1a0e6c7596050261b0a36f4aa8632c3fcb56a55
thatguy027 <szdralje@gmail.com>
2019-06-10 17:04:26 -0500

    add pc correlation script

--- Scripts/Plot_PCs.R
+++ Scripts/Plot_PCs.R
@@ -1,79 +1,75 @@
 library(tidyverse)
+library(clusterProfiler)
+library(org.Ce.eg.db)
+
+options(scipen=999)
 
 setwd(glue::glue("{dirname(rstudioapi::getActiveDocumentContext()$path)}/.."))
 
+masked_n_remove <- "Data/EIGENSTRAT/DIVERGENT-MASKED/NO_REMOVAL/eigenstrat_no_removal.evac"
+masked_y_remove <- "Data/EIGENSTRAT/DIVERGENT-MASKED/OUTLIER_REMOVAL/eigenstrat_outliers_removed.evac"
 
+intersect_n_remove <- "Data/EIGENSTRAT/GATK-STRELKA_Intersection/NO_REMOVAL/eigenstrat_no_removal.evac"
+intersect_y_remove <- "Data/EIGENSTRAT/GATK-STRELKA_Intersection/OUTLIER_REMOVAL/eigenstrat_outliers_removed.evac"
 
-
-
-pc_masked <- data.table::fread("~/transfer/popgen/Masked_eigenstrat_no_removal.evac",skip = 1, header = F) %>%
-  dplyr::arrange(V1)
-
-colnames(pc_masked) <- c("Strain", paste0("PC", 1:(ncol(pc_masked)-1))) 
-
-
-ggplot(pc_masked)+
-  aes(x = PC4, y = PC5)+
-  geom_point()+
-  theme_classic(18)
-
-
-pcs <- data.table::fread("~/transfer/popgen/eigenstrat_no_removal.evac",skip = 1, header = F)%>%
-  dplyr::arrange(V1)
-
-colnames(pcs) <- c("Strain", paste0("PC", 1:(ncol(pcs)-1))) 
-
-
-ggplot(pcs)+
-  aes(x = PC4, y = PC5)+
-  geom_point()+
-  theme_classic(18)
-
-
-pc_cor <- c()
-for(pcc in 2:51){
-  pc_cor <- append(x = pc_cor,cor(pcs[,pcc],pc_masked[,pcc], method = "spearman"))
+compare_pca <- function(masked, intersection, removal = F) {
+  
+  pca_masked <- data.table::fread(masked, skip = 1, header = F) %>%
+    dplyr::arrange(V1)
+  
+  n_pcs <- ncol(pca_masked)-1
+  pca_masked <- pca_masked[,c(1:n_pcs)]
+  colnames(pca_masked) <- c("Strain", paste0("PC", 1:(n_pcs-1)))
+  
+  pca_intersect <- data.table::fread(intersection, skip = 1, header = F) %>%
+    dplyr::arrange(V1)
+  
+  n_pcs <- ncol(pca_intersect)-1
+  pca_intersect <- pca_intersect[,c(1:n_pcs)]
+  
+  colnames(pca_intersect) <- c("Strain", paste0("PC", 1:(n_pcs-1)))
+  
+  if(removal == F) {
+    pc_cor <- c()
+    for(pcc in 2:(ncol(pca_intersect))){
+      pc_cor <- append(x = pc_cor, cor(pca_masked[,pcc],pca_intersect[,pcc], method = "spearman"))
+    }
+    
+    pc_cor <- data.frame(PC_COR = pc_cor, PC = paste0("PC", 1:(n_pcs-1)))
+  } else {
+    
+    pca_masked <- pca_masked %>% dplyr::filter(Strain%in%pca_intersect$Strain)
+    pca_intersect <- pca_intersect %>% dplyr::filter(Strain%in%pca_masked$Strain)
+    
+    pc_cor <- c()
+    for(pcc in 2:51){
+      pc_cor <- append(x = pc_cor, cor(pca_masked[,pcc],pca_intersect[,pcc], method = "spearman"))
+    }
+    
+    pc_cor <- data.frame(PC_COR = pc_cor, PC = paste0("PC", 1:(n_pcs-1)))
+    
   }
-
-pc_cordf <- data.frame(pc_cor = pc_cor, pc = 1:length(pc_cor))
-
-ggplot(pc_cordf)+
-  aes(x = pc, y = abs(pc_cor))+
-  geom_point()
-
-pc_masked <- data.table::fread("~/transfer/popgen/Masked_eigenstrat_outliers_removed.evac",skip = 1, header = F) %>%
-  dplyr::arrange(V1)
-
-colnames(pc_masked) <- c("Strain", paste0("PC", 1:(ncol(pc_masked)-1))) 
-
-
-ggplot(pc_masked)+
-  aes(x = PC3, y = PC4)+
-  geom_point()+
-  theme_classic(18)
-
-
-pcs <- data.table::fread("~/transfer/popgen/eigenstrat_outliers_removed.evac",skip = 1, header = F)%>%
-  dplyr::arrange(V1)
-
-colnames(pcs) <- c("Strain", paste0("PC", 1:(ncol(pcs)-1))) 
-
-
-ggplot(pcs)+
-  aes(x = PC3, y = PC4)+
-  geom_point()+
-  theme_classic(18)
-
-pc_match <- pcs %>% dplyr::filter(Strain%in%pc_masked$Strain)
-pcm_match <- pc_masked %>% dplyr::filter(Strain%in%pcs$Strain)
-
-pc_cor <- c()
-for(pcc in 2:51){
-  pc_cor <- append(x = pc_cor,cor(pcm_match[,pcc],pc_match[,pcc], method = "spearman"))
+  
+  return(pc_cor)
 }
 
-pc_cordf <- data.frame(pc_cor = pc_cor, pc = 1:length(pc_cor))
+no_removal_pca_cor <- compare_pca(masked_n_remove, intersect_n_remove)
 
-ggplot(pc_cordf)+
-  aes(x = pc, y = abs(pc_cor))+
-  geom_point()
+ggplot(no_removal_pca_cor)+
+  aes(x = factor(PC, levels  = PC), y = abs(PC_COR))+
+  geom_point() +
+  theme_classic(18) +
+  theme(axis.text.x = element_text(angle = 90))+
+  labs(y = "Spearman's Correlation", x = "Principal Component")
+
+
+removal_pca_cor <- compare_pca(masked_y_remove, intersect_y_remove, removal = T)
+
+ggplot(removal_pca_cor)+
+  aes(x = factor(PC, levels  = PC), y = abs(PC_COR))+
+  geom_point() +
+  theme_classic(18) +
+  theme(axis.text.x = element_text(angle = 90))+
+  labs(y = "Spearman's Correlation", x = "Principal Component")
+
+
